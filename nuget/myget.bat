55b48383ca0d84bd36100f4e266f296f30cf6ce0
Dale Newman <dalenewman@gmail.com>
2020-07-04 17:05:35 -0400

    Added .NET Standard 2.0 Targets

---
+++ nuget/myget.bat
@@ -0,0 +1,2 @@
+nuget push "c:\temp\modules\Transformalize.Transform.LambdaParser.0.8.15-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.LambdaParser.Autofac.0.8.15-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
